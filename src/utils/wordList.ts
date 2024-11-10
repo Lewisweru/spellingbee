6e1346a3288ae7c52aa0a2b3ba68a727acdbc2cd
Kish <125549771+Lewisweru@users.noreply.github.com>
2024-11-11 00:50:45 +0300

    Create wordList.ts

---
+++ src/utils/wordList.ts
@@ -0,0 +1,11 @@
+export const wordList = {
+  easy: [
+    "cat", "dog", "sun", "tree", "eat" , "ball", "book", "fish", "milk", "apple", "star"
+  ],
+  medium: [
+    "giraffe", "chicken", "puzzle", "library", "mountain", "diamond", "cucumber", "umbrella", "sandwich", "elephant"
+  ],
+  hard: [
+    "accommodate", "boulevard", "conscientious", "miscellaneous", "pharaoh", "quarantine", "rhythm", "sacrilegious", "silhouette", "xylophone"
+  ]
+};
