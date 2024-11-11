30100bc134133f7384f76a8f8433ce84b47522a4
Kish <125549771+Lewisweru@users.noreply.github.com>
2024-11-11 20:46:25 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'; 
-import App from './App'; 
-import './index.css'; 
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </StrictMode>
-  );
-}
+    </BrowserRouter>
+  </React.StrictMode>
+);
