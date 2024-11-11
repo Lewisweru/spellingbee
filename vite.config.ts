08e39086d0f131a096d42e98646c8b5009aaa7d3
Kish <125549771+Lewisweru@users.noreply.github.com>
2024-11-11 19:37:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'], // Keep excluding lucide-react if it's causing issues
-    include: ['react-router-dom'], // Explicitly include react-router-dom for optimization
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Make sure react-router-dom is treated as an external dependency
-    },
+    include: ['react-router-dom'],
   },
 });
