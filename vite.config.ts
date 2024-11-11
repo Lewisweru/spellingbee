156826a33f40589f3d77ff863f7e9bca9f032be5
Kish <125549771+Lewisweru@users.noreply.github.com>
2024-11-11 19:47:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Add react-router-dom as external
-    },
-  },
   optimizeDeps: {
     include: ['react-router-dom'], // Ensure react-router-dom is included in dev build
   },
